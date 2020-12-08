fddb92f57123b5815b23fdfdff0398bbc839760d
Andy Hinrichs <hinrichsad@gmail.com>
2020-12-08 18:29:38 -0500

    -

--- end.js
+++ end.js
@@ -1,3 +1,4 @@
+
 //gets score from the last taken quiz 
 var mostRecentScore = localStorage.getItem("mostRecentScore");
 JSON.parse(mostRecentScore);
@@ -7,54 +8,55 @@ console.log(mostRecentScore);
 
 $(document).ready(function () {
 
-    var getScores = localStorage.getItem("newBoard");
-    JSON.parse(newBoard);
-
-    if (getScores !== null) {
-        highScores = newBoard;
-    }
-    console.log(highScores);
     $("#finalScore").text(mostRecentScore);
 
 
-    //takes input and saves the name and most recent score to local storage,
-    //then turns them into an object and pushes them onto the previus Scores board
+//takes input and saves the name and most recent score to local storage,
+//then turns them into an object and pushes them onto the previus Scores board
 
-    $("#submit").on("click", function () {
-        var name = $("#name").val();
-        var myScore = mostRecentScore;
+$("#submit").on("click", function () {
+    var name = $("#name").val();
+    var myScore = mostRecentScore;
 
-        localStorage.setItem("name", name);
-        localStorage.setItem("score", myScore);
+    localStorage.setItem("name", name);
+    localStorage.setItem("score", myScore);
 
-        var scoreInput = {
-            name: name,
-            score: myScore
-        }
-
-        var newBoard = highScores.unshift(scoreInput);
-        console.log(newboard);
-
-
-        storeObj();
-        returnToStart();
-    });
-
-    function storeObj() {
-
-        localStorage.setItem('newBoard', JSON.stringify(newBoard));
+    var scoreInput = {
+        name: name,
+        score: myScore
     }
 
-    $("#restart").on("click", function () {
-        storeObj();
-        returnToStart();
-    });
-
-    function returnToStart() {
-        window.location.href = "index.html";
-    }
-    var highScores = [];
-
+    highScores.unshift(scoreInput);
     console.log(highScores);
 
+
+    storeObj();
+    returnToStart();
 });
+
+function storeObj(){
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+}
+
+$("#restart").on("click", function () {
+    storeObj();
+    returnToStart();
+});
+
+function returnToStart(){
+    window.location.href = "index.html";
+}
+var highScores = [];
+
+console.log(highScores);
+
+});
+
+var getScores = localStorage.getItem("highScores");
+JSON.parse(getScores);
+
+if(getScores !== null){
+    getScores = highScores;
+}
+console.log(highScores);
